58586dc91efec21813ab1a718541053252a5f26f
Francisco Dominguez <francisco.dominguez@urjc.es>
2015-10-09 17:54:50 +0200

    Inicio

---
+++ cTelas/src/cTelas.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cTelas.cpp
+// Author      : Francisco Dominguez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World GIT!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
