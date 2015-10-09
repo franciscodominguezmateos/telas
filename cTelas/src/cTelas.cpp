c10087a59fd3e3106bc22fc05e9a1a94b0f75c2f
Francisco Dominguez <francisco.dominguez@urjc.es>
2015-10-09 20:11:34 +0200

    Idle

--- cTelas/src/cTelas.cpp
+++ cTelas/src/cTelas.cpp
@@ -7,12 +7,18 @@
 //============================================================================
 
 #include <iostream>
+#include <unistd.h>
 #include <GL/glut.h>
 using namespace std;
+
+float newAngle=0;
 
 void displayMe(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -10.0f);
+    glRotatef(newAngle += 1, 1.0f, 0.0f, 0.0f);
     glBegin(GL_POLYGON);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(0.5, 0.0, 0.0);
@@ -22,7 +28,11 @@ void displayMe(void)
     //glFlush();
     glutSwapBuffers();
 }
-
+void managerIdle(void)
+{
+    //usleep(1000);
+    glutPostRedisplay();
+}
 void managerKeyboard(unsigned char key, int x, int y)
 {
     switch (key)
@@ -35,7 +45,23 @@ void managerKeyboard(unsigned char key, int x, int y)
     (void)(x);
     (void)(y);
 }
-
+void managerMouse(int button, int state, int x, int y)
+{
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(managerIdle);
+        break;
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(NULL);
+        break;
+        default:
+        break;
+    }
+    (void)(x);
+    (void)(y);
+}
 void managerResize(int w, int h) {
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
@@ -45,16 +71,17 @@ void managerResize(int w, int h) {
 }
 
 int main(int argc, char** argv) {
-	cout << "!!!Hello World GIT OpenGL!!!" << endl; // prints !!!Hello World!!!
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glEnable(GL_DEPTH_TEST);
     glutInitWindowSize(300, 300);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world :D");
+    glutCreateWindow("Hello world :D GIT!!!");
     glutDisplayFunc(displayMe);
-    glutKeyboardFunc(managerKeyboard);
     glutReshapeFunc(managerResize);
+    glutMouseFunc(managerMouse);
+    glutKeyboardFunc(managerKeyboard);
+    glutIdleFunc(managerIdle);
 
     glutMainLoop();
     return 0;
