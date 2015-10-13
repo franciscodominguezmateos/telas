87ef243c25f8cfef6589f0fee3b43a55be7b1912
Francisco Dominguez <francisco.dominguez@urjc.es>
2015-10-13 21:08:21 +0200

    Eigen included

--- cTelas/src/cTelas.cpp
+++ cTelas/src/cTelas.cpp
@@ -11,9 +11,12 @@
 #include <GL/glut.h>
 #include <cv.h>
 #include <highgui.h>
+#include <Eigen/Dense>
 
 using namespace std;
 using namespace cv;
+
+using Eigen::MatrixXd;
 
 float newAngle=0;
 
@@ -101,5 +104,11 @@ int main(int argc, char** argv) {
 
     waitKey(0);
     glutMainLoop();
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
     return 0;
 }
